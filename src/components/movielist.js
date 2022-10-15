0a34e28ee94131f07448c8e056ef788516b97cea
atigfadi <fadiatig3@gmail.com>
2022-10-15 15:46:39 +0100

    first commit

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,8 +1,15 @@
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.css';
-import ReactStars from "react-rating-stars-component"
+import ReactStars from "react-rating-stars-component";
 import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReadMoreReact from "read-more-react";
+
+
+
+
 
 const Movielist = ({movies}) =>{
   const[searchmov,setsearchmov]=useState('')
@@ -19,15 +26,32 @@ const Movielist = ({movies}) =>{
         {movies.filter(el=>el.Title.toUpperCase().includes(searchmov.toUpperCase())&& el.Rating>=searchrating).map((el)=>(
         <div style={{margin:'auto',textAlign:'center',marginTop:'10px'}} >
     <Card style={{width: '18rem'}}>
-      <a href={el.link}><Card.Img variant="top" src={el.Poster} /></a>
+    <Card.Img variant="top" src={el.Poster} />
+
       <Card.Body>
         <Card.Title>{el.Title}</Card.Title>
         <Card.Title>{el.Year}</Card.Title>
-        <Card.Text>{el.Description}</Card.Text>
+        <Card.Text>
+        <ReadMoreReact
+                            text={el.Description}
+                            min={80}
+                            ideal={100}
+                            max={120}
+                            readMoreText="Read more"
+                           
+                        />
+          
+          
+          </Card.Text>
         <div style={{marginLeft:'5rem'}}>
         <ReactStars value={parseInt(el.Rating)/2} edit={false}isHalf={true}/></div>
       </Card.Body>
+      <a href={el.link}> <Button> Trailer</Button></a>
+      
+     
+
     </Card>
+
        </div>
  ))}  </div>
     </div>
